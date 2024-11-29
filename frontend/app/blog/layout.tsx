cd5b5bada4d920c6bef9aaa542437764a809e8f4
Pasakorn Juipiam <pasakornjuipiam@Pasakorns-MacBook-Pro.local>
2024-11-29 17:49:48 +0700

    adjust layout

--- frontend/app/blog/layout.tsx
+++ frontend/app/blog/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Navbar } from "../components/navbar";
+import { SideNav } from "../components/sidenav";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div>{children}</div>
+        <Navbar></Navbar>
+        <SideNav />
+        <div className="ml-[128px]">{children}</div>
       </body>
     </html>
   );
--- frontend/app/blog/our/page.tsx
+++ frontend/app/blog/our/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 // import axios from "axios";
 import { useEffect, useState } from "react";
-import { Card } from "../components/card";
-import { Input } from "../components/input";
-import { Button } from "../components/button";
-import { Modal } from "../components/modal";
-import { TextArea } from "../components/text";
+import { Card } from "../../components/card";
+import { Input } from "../../components/input";
+import { Select } from "../../components/select";
+import { Button } from "../../components/button";
+import { Modal } from "../../components/modal";
+import { TextArea } from "../../components/text";
 type PostProps = {
   id: string;
   title: string;
@@ -14,7 +15,7 @@ type PostProps = {
 export default function Blog() {
   const [posts, setPosts] = useState<PostProps[]>([]);
   const [isCreateModal, setIsCreateModal] = useState(false);
-
+  const [isEditModal, setIsEditModal] = useState(false);
   useEffect(() => {
     const mockPost = [
       {
@@ -37,12 +38,25 @@ export default function Blog() {
   const onHandleCreateModal = () => {
     setIsCreateModal(!isCreateModal);
   };
+  const onHandleEditModal = () => {
+    setIsEditModal(!isEditModal);
+  };
 
   return (
     <div className="grid grid-cols-1">
       <div className="mb-2 flex">
+        <Input
+          type="text"
+          className="w-50 mt-4 w-[200px]"
+          placeholder="Search"
+        />{" "}
+        <Select className="w-50 mt-4 w-[200px]">
+          <option value="0">Comunity</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+        </Select>
         <Button onClick={onHandleCreateModal} className="w-50 mt-4">
-          Create
+          Search
         </Button>
       </div>
       {isCreateModal && (
@@ -60,12 +74,28 @@ export default function Blog() {
           </div>
         </Modal>
       )}
+      {isEditModal && (
+        <Modal>
+          <div className="bg-white w-full mb-1">
+            <h1 className="text-xl font-bold">Edit Post</h1>
+            <Input type="text" className="w-full mt-4" placeholder="Title" />
+            <TextArea className="w-full mt-4" placeholder="Detail" />
+            <div className="mt-4 flex justify-between">
+              <Button className="bg-green-400 mr-1">Edit</Button>
+              <Button onClick={onHandleEditModal} className="bg-red-400">
+                Cancel
+              </Button>
+            </div>
+          </div>
+        </Modal>
+      )}
       {posts.map((post: PostProps) => (
         <Card
           id={post.id}
           key={post.id}
           title={post.title}
           detail={post.detail}
+          onEdit={onHandleEditModal}
         />
       ))}
     </div>
