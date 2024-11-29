cc2a730de6eb810bcd6a9e5ed1f786b9eae51dc7
Pasakorn Juipiam <pasakornjuipiam@Pasakorns-MacBook-Pro.local>
2024-11-29 19:39:49 +0700

    adding functoin comment BE

--- frontend/app/blog/dashboard/[slug]/page.tsx
+++ frontend/app/blog/dashboard/[slug]/page.tsx
@@ -11,13 +11,15 @@ type PostProps = {
   detail: string;
   comments: {
     id: string;
-    comment: string;
-    authorName: string;
+    detail: string;
+    authorId: string;
   }[];
 };
 export default function PostSlug() {
   const [post, setPost] = useState<PostProps>();
   const params = useParams();
+  const [comment, setComment] = useState("");
+
   useEffect(() => {
     const { slug } = params;
     axios
@@ -31,20 +33,53 @@ export default function PostSlug() {
       });
   }, []);
 
+  const handleComment = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(e.target.value);
+  };
+
+  const handleCommentSubmit = () => {
+    const { slug } = params;
+    axios
+      .post(
+        `http://localhost:3000/comments`,
+        {
+          detail: comment,
+          postId: slug,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${Cookies.get("access_token")}`,
+          },
+        }
+      )
+      .then((response) => {
+        window.location.reload();
+      });
+  };
   return (
     <div className="flex justify-center mt-10">
       <div className=" w-[375px]">
         <h1 className="text-bold">{post?.title}</h1>
         <p>{post?.detail}</p>
         <div className="w-[200px]">
-          <TextArea className="w-full mt-4" placeholder="Comment" />
-          <Button className="bg-green-400">Comment</Button>
+          <TextArea
+            className="w-full mt-4"
+            placeholder="Comment"
+            value={comment}
+            onChange={handleComment}
+          />
+          <Button className="bg-green-400" onClick={handleCommentSubmit}>
+            Comment
+          </Button>
         </div>
+
         <div className="w-[200px]">
+          <hr className="mt-4" />
           {post?.comments.map((comment) => (
             <div key={comment.id} className="block">
-              <div className="mb-2 ">{comment.authorName}</div>
-              {comment.comment}
+              <div className="mb-2 "></div>
+              {comment.detail}
+              <hr className="mt-4" />
             </div>
           ))}
         </div>
