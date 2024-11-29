3a41bb760c39e3c0f680e3ad49300eb3c4456ab3
Pasakorn Juipiam <pasakornjuipiam@Pasakorns-MacBook-Pro.local>
2024-11-29 13:03:35 +0700

    adding comment and post structure

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, PostsModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
